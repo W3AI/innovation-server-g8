7567415633e540f5f87637bf04596a09e20a9841
stefian <v_ianta@yahoo.com>
2019-04-24 15:06:29 -0400

    added mongoose, db and post model

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -17,4 +18,9 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect(
+  'mongodb+srv://steve_ai91:o82HN0KCxu6dtS5w@cluster0-n7ze3.mongodb.net/messages?retryWrites=true',
+  { useNewUrlParser: true }
+  ).then(result => {
+    app.listen(8080);
+  }).catch(err => console.log(err));
